f41614e3b93fdddac7bb179dcccc6e4ad6f91ce1
aaa <fefema1234@gmail.com>
2022-12-22 22:17:22 +0200

    linux account managment

---
+++ bash/fundamentals/03/03_linux_account_managment/01_password.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cat /etc/passwd # testuser:x:3000:3000:LPI tutorial test user:/home/testuser:/bin/bash
+
+cat /etc/shadow # Password value -> drobbins:$1$1234567890123456789012345678901:11664:0:-1:-1:-1:-1:0
